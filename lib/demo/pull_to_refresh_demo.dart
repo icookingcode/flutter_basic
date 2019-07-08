7be84f8395325ffba6dd917d207e42b1a48c859d
guchaochao <guchaochao@huiyunit.com>
2019-07-08 15:59:33 +0800

    下拉刷新提示语修改

--- lib/demo/pull_to_refresh_demo.dart
+++ lib/demo/pull_to_refresh_demo.dart
@@ -11,7 +11,8 @@ class PullToRefreshDemo extends StatefulWidget {
 }
 
 class _PullToRefreshDemoState extends State<PullToRefreshDemo> {
-  final List<Product> _list = List.generate(20, (index)=>Product('商品$index', '这是第$index个商品的详情'));
+  final List<Product> _list =
+      List.generate(20, (index) => Product('商品$index', '这是第$index个商品的详情'));
   RefreshController _controller;
 
   @override
@@ -27,16 +28,23 @@ class _PullToRefreshDemoState extends State<PullToRefreshDemo> {
     _controller.dispose();
     super.dispose();
   }
+
   void _onRefresh() {
-    new Timer(Duration(seconds: 2,), (){
+    new Timer(
+        Duration(
+          seconds: 2,
+        ), () {
       _controller.refreshCompleted();
     });
-
   }
 
   void _onLoading() {
-    new Timer(Duration(seconds: 2,), (){
+    new Timer(
+        Duration(
+          seconds: 2,
+        ), () {
       _controller.loadComplete();
+      _controller.loadNoData();
     });
   }
 
@@ -50,21 +58,34 @@ class _PullToRefreshDemoState extends State<PullToRefreshDemo> {
         controller: _controller,
         enablePullDown: true,
         enablePullUp: true,
-        header: ClassicHeader(),
+        header: ClassicHeader(
+          refreshingText: '正在刷新，请稍后...',
+          idleText: '下拉刷新',
+          releaseText: '释放刷新',
+          completeText: '已刷新',
+          failedText: '刷新失败',
+        ),
 //        header: WaterDropMaterialHeader(),
+        footer: ClassicFooter(
+          loadingText: '加载中...',
+          idleText: '加载更多',
+          failedText: '加载失败，点击重试',
+          noDataText: '已加载完',
+        ),
         onRefresh: _onRefresh,
         onLoading: _onLoading,
-        child:  ListView.builder(itemBuilder: _buildItem,itemCount: _list.length,),
+        child: ListView.builder(
+          itemBuilder: _buildItem,
+          itemCount: _list.length,
+        ),
       ),
     );
   }
 
-  Widget _buildItem(BuildContext context,int index){
-
+  Widget _buildItem(BuildContext context, int index) {
     return ListTile(
       title: Text(_list[index].title),
-      onTap: (){
-      },
+      onTap: () {},
     );
   }
 }
